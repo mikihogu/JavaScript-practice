c8fc6451dfbf88bb2ddb399e2472da3cbb1e56d0
mikihogu <mk.i.ph93@gmail.com>
2023-04-17 23:54:58 +0900

    color picker2

--- ColorPicker/js/script.js
+++ ColorPicker/js/script.js
@@ -1,20 +1,7 @@
-const text = document.querySelector('#colorText');
-const color = document.querySelector('#colorPicker');
+console.log(document.querySelector('#colorPicker').value);
 
-// カラーピッカーを操作した時の動作
-const colorBg = () => {
-  // 選択したカラーを背景色に設定
-  document.body.style.backgroundColor = color.ariaValueMax;
 
-  // カラーコードを表示
-  if (color.value === '#ffffff') {
-    text.textContent = `カラーコード： ${color.value} (white)`;
-  } else if (color.value === '#000000') {
-    text.textContent = `カラーコード： ${color.value} (black)`;
-  } else {
-    text.textContent = `カラーコード： ${color.value}`;
-  }
-}
+// document.querySelector('#colorText').textContent = 'カラーコード：' + document.querySelector('#colorPicker').value;
+document.querySelector('#colorText').textContent = `カラーコード : ${document.querySelector('#colorPicker').value}`;
+document.querySelector('#colorPicker');
 
-// カラーピッカーが変更されたら colorBg を実行
-color.addEventListener('input', colorBg);
