3a9dc7c5a335770864063b3e724ac9cc5ded7068
mikihogu <mk.i.ph93@gmail.com>
2023-04-27 12:31:39 +0900

    scroll2

--- scroll/js/script.js
+++ scroll/js/script.js
@@ -1,6 +1,14 @@
 const getScrollPercent = () => {
   const scrolled = window.scrollY;
-  console.log(`${scrolled} スクロール`);
+  // console.log(`${scrolled} スクロール`);
+
+  //ページの高さ
+  const pageHeight = document.documentElement.scrollHeight;
+  //表示領域の高さ
+  const viewHeight = document.documentElement.clientHeight;
+
+  const percentage = scrolled / (pageHeight - viewHeight) * 100;
+  console.log(percentage);
 }
 
 window.addEventListener('scroll', getScrollPercent);
