56fcee126b685f38806cd8434f47ef976ff2a9ef
mikihogu <mk.i.ph93@gmail.com>
2023-04-28 22:46:32 +0900

    pics1

---
+++ pics/js/script.js
@@ -0,0 +1,5 @@
+const menu = document.querySelector('#menu');
+
+const content = `<div><img src="images/001.JPG" alt=""></div>`;
+menu.insertAdjacentHTML('beforeend', content);
+// menu.textContent = content;
