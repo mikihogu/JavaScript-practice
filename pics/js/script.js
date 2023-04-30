c0f0614ec796ff24e72a9d4d324adb9d6dca82b9
mikihogu <mk.i.ph93@gmail.com>
2023-04-30 21:43:18 +0900

    pics2

--- pics/js/script.js
+++ pics/js/script.js
@@ -1,5 +1,22 @@
 const menu = document.querySelector('#menu');
 
-const content = `<div><img src="images/001.JPG" alt=""></div>`;
+const lists = [
+  '001.JPG',
+  '002.JPG',
+  '003.JPG',
+  '004.JPG',
+  '005.JPG',
+  '006.JPG',
+];
+// console.log(lists[2]);
+
+const content = `<div><img src="images/${lists[0]}" alt=""></div>
+  <div><img src="images/${lists[1]}" alt=""></div>
+  <div><img src="images/${lists[2]}" alt=""></div>
+  <div><img src="images/${lists[3]}" alt=""></div>
+  <div><img src="images/${lists[4]}" alt=""></div>
+  <div><img src="images/${lists[5]}" alt=""></div>
+`; 
+// const content = `<div><img src="images/001.JPG" alt=""></div>`;
 menu.insertAdjacentHTML('beforeend', content);
-// menu.textContent = content;
+
